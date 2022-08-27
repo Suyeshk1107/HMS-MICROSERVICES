1fc1eec3958a0de41830059048e00fc437e71452
suyesh.kumar123@gmail.com <suyesh.kumar123@gmail.com>
2022-08-28 00:22:38 +0530

    doctor updated

--- Doctor-Service/src/main/java/com/doctor/resource/DoctorResource.java
+++ Doctor-Service/src/main/java/com/doctor/resource/DoctorResource.java
@@ -2,8 +2,11 @@ package com.doctor.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.doctor.bean.Doctor;
@@ -15,12 +18,6 @@ public class DoctorResource {
 	
 	@Autowired
 	private DoctorService doctorService;
-
-	
-//	@GetMapping(path = "/shares/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
-//	public Share getShareResource(@PathVariable("id") int shareId) {
-//		return shareService.getShareById(shareId);
-//	}
 	
 	@GetMapping(path = "/doctors", produces = MediaType.APPLICATION_JSON_VALUE)
 	public DoctorList getAllDoctors() {
@@ -32,7 +29,12 @@ public class DoctorResource {
 		return doctorService.getDoctorById(doctorId);
 	}
 	
-	@GetMapping(path = "/doctors/remove/{dId}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(path = "/doctors", produces = MediaType.APPLICATION_JSON_VALUE)
+	public boolean saveEmployeeResource(@RequestBody Doctor doctor) {
+		return doctorService.addDoctor(doctor);
+	}
+	
+	@DeleteMapping(path = "/doctors/remove/{dId}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public String removeDoctorById(@PathVariable("dId") String doctorId) {
 		if(doctorService.deleteDoctor(doctorId)) {
 			return "Doctor Deleted Successfully !";
