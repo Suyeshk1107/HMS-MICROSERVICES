a28b72cd2be2f334fba77825268c864272941736
krishna-kusum <krishnamodi41423@gmail.com>
2022-09-10 15:18:28 +0530

    added test for login service in hms client

---
+++ HMS-Client/src/test/java/com/service/LoginTest.java
@@ -0,0 +1,82 @@
+package com.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
+import com.bean.Login;
+import com.service.LoginService;
+
+@ExtendWith(MockitoExtension.class)
+class LoginTest {
+
+	@Autowired
+	@InjectMocks
+	private LoginService loginService;
+	
+	@Mock
+	private RestTemplate restTemplate;
+	
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+//	@Test
+//	void testGetLogin() {
+////		fail("Not yet implemented");
+//		loginService.getLogin("D1000");
+//	}
+//
+//	@Test
+//	void testSaveLogin() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	void testDeleteLogin() {
+//		fail("Not yet implemented");
+//	}
+	
+	@Nested
+	class TestGetLogin{
+		
+		@Test
+		void testGetLogin_T001() {
+			
+			Login login = new Login("D1000","D1000");
+			
+			Mockito.when(restTemplate.getForObject("http://login-service/logins/" + "D1000", Login.class )).thenReturn(login);
+			
+			assertEquals(login, loginService.getLogin("D1000"));
+			
+		}
+		
+		
+	}
+
+}
